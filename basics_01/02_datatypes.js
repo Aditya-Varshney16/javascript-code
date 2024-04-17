a9b2eafd350f5d1bc6424c91f9cd9cef543c4364
Aditya-Varshney16 <167291578+Aditya-Varshney16@users.noreply.github.com>
2024-04-17 20:47:03 +0000

    commit2

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,19 @@
+"usre strict"; // treat all js code as newer version
+
+// alert is used only in browser not in node
+
+let name="Aditya"
+let age=21
+let isLoggedIn=true
+
+/*
+nummber => 2 to the power 53
+bigint 
+string =>  " "
+boolean=> true/false
+null => standalone value
+undefined => when the value of a variable is not given
+*/
+
+console.log(typeof null); // => Object 
+console.log(typeof undefined); // => Undefined
---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)  // => type conversion
+console.log(typeof valueInNumber);
+
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1
+false => 0
+*/
+
+/*
+"" => false
+"aditya" => true
+*/
